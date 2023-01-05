022f085752fc1984e397aceeb865115add18309b
unknown <obidullahbinmasud124@gmail.com>
2023-01-05 18:23:49 +0600

    show name and create start practice button and style practice button

--- src/components/QuizData/QuizData.js
+++ src/components/QuizData/QuizData.js
@@ -3,12 +3,24 @@ import React from 'react';
 import './QuizData.css'
 
 const QuizData = ({ quiz }) => {
-    const { logo, name } = quiz;
+    const { logo, name, total } = quiz;
+    console.log(quiz)
     return (
         <div className='quizdata'>
-            <div className='quizdata-show'>
-                <img src={logo} alt="" />
-                <p>{name}</p>
+            <div className='quiz-show-container'>
+
+                <div className='quizdata-show'>
+                    <img src={logo} alt="" />
+                </div>
+
+                <div>
+                    <p className='text-3xl text-left font-bold text-slate-700'>{name}</p>
+                    <p className='text-2xl text-left font-bold text-slate-700'>Total quiz are {total}</p>
+                </div>
+
+            </div>
+            <div className='practice-button w-full rounded-md'>
+                <button className='p-1 font-bold text-2xl'>Start practice</button>
             </div>
         </div>
     );
