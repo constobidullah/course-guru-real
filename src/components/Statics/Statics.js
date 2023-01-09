2c1bbf642ef066835d3651e1e1fb03b6d35fe034
unknown <obidullahbinmasud124@gmail.com>
2023-01-09 22:26:41 +0600

    rechart added

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Chart from '../Chart/Chart';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const Statics = () => {
     const quizData = useLoaderData();
+
     return (
         <div>
-            {
-                quizData.data.map(quiz => <Chart
-                    key={quiz.id}
-                    quiz={quiz}
-                ></Chart>)
-            }
+                <LineChart width={1400} height={500} data={quizData.data}>
+                    <Line type="monotone" dataKey="total" stroke='#FD5901'></Line>
+                    <YAxis></YAxis>
+                    <XAxis dataKey="name" stroke='#0000a0' className='font-semibold'/>
+                    <Tooltip></Tooltip>
+                    <CartesianGrid></CartesianGrid>
+                </LineChart>
         </div>
     );
 };
